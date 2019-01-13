e490b78e2f6af0313bda271e1ee065c78bed30f0
unknown <mimo3a@gmail.com>
2019-01-13 20:43:21 +0200

    Demianov HW2

--- src/homework.ts
+++ src/homework.ts
@@ -1,2 +1,36 @@
-// ***************** Этот файл можно удалить если он вам не подходит или не нужен. ***************** 
-// *************************************************************************************************
+abstract class Car {
+    protected mileage: number;
+    protected fuel: number;
+    
+    public abstract drive(): void;
+    public abstract refuel(): void;
+    }
+
+    class Ford extends Car{
+        
+        constructor(mileage: number, fuel: number) {
+            super();
+            this.mileage = mileage;
+            this.fuel = fuel
+        }
+         public drive(distance: number):void {
+            this.mileage -= distance;
+            this.fuel -= distance * 0.05;
+            if (this.fuel <= 0) {
+                console.log("Вам необходимо запрввиться");
+            }
+       } 
+        public refuel (liters: number): void {
+           this.fuel += liters;
+       }  
+       public get mileage(): number {
+           return this.mileage;
+       }
+       public get fuel(): number {
+           return this.fuel;
+       }
+
+        }
+    
+
+
